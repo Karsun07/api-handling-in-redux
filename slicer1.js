ab9320585123524aeb22fdcc59706a2f7053d2e2
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-03-29 11:30:42 +0530

    createAsyncThunk

--- slicer1.js
+++ slicer1.js
@@ -1,7 +1,27 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit";
+
+
+// createAsyncThunk
+
+// {type: 'coin/fetch/pending' , payload: undefined}
+// {type: 'Coin/fetch/fullfiled' , payload:data}
+// {type: 'Coin/fetch/rejected, payload: "error_message"}
+
 
 const FetchData=createAsyncThunk(
     
-    'Coin/fetch',
+   'Coin/fetch',
+//    we created an action type:coin/fetch
+   async(args,thunkAPI)=>{
+         try{
+            const response=await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${args}`);
+            const data=await response.json();
+            return data;
+         }
+         catch(error){
+            return rejectWithValue(error.message);
+         }
+  
+   }
 
 )
