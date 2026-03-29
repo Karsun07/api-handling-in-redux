4a883df2019243fe6429bdce8a86770ddf9203f0
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-03-29 12:16:15 +0530

    coincard created

--- CoinCreate.js
+++ CoinCreate.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { FetchData } from "./slicer1";
-import CoinCard from "./CoinCard";
+import CoinCard from "./";
 
 export default function CoinCreate(){
     
